e414b8b1ae5724ec970b0d4db2a7cf8a3ff50ab2
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-04-06 16:29:56 +0100

    Initial Commit

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,30 +1,36 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
-import { BackButton } from '../components/BackButton';
-import Details from '../screens/details';
-import Overview from '../screens/overview';
+import Home from './(tabs)/home';
+import Settings from './(tabs)/settings';
 
-export type RootStackParamList = {
-  Overview: undefined;
-  Details: { name: string };
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
+// const Stack = createStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
 
 export default function RootStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Overview">
-        <Stack.Screen name="Overview" component={Overview} />
-        <Stack.Screen
-          name="Details"
-          component={Details}
-          options={({ navigation }) => ({
-            headerLeft: () => <BackButton onPress={navigation.goBack} />,
-          })}
-        />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+// export default function RootStack() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Overview">
+//         <Stack.Screen name="Overview" component={Overview} />
+//         <Stack.Screen
+//           name="Details"
+//           component={Details}
+//           options={({ navigation }) => ({
+//             headerLeft: () => <BackButton onPress={navigation.goBack} />,
+//           })}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
