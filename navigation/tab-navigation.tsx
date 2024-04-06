e59e6e4f6e3c7b2ee41968475ff0223506fa324b
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-04-07 00:08:20 +0100

    tab icons and label styles

--- navigation/tab-navigation.tsx
+++ navigation/tab-navigation.tsx
@@ -1,10 +1,10 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StackScreenProps } from '@react-navigation/stack';
-// import { Heart, Home2, Notification, ShoppingBag } from 'iconsax-react-native';
-// import { StyleSheet } from 'react-native';
+import { Map, ProfileCircle, Setting3 } from 'iconsax-react-native';
 
 import { RootStackParamList } from '.';
 import Home from './(tabs)/home';
+import Profile from './(tabs)/profile';
 import Settings from './(tabs)/settings';
 
 const Tab = createBottomTabNavigator();
@@ -18,6 +18,7 @@ export default function TabNavigator({ navigation }: Props) {
         tabBarActiveTintColor: 'orange',
         tabBarStyle: {
           paddingTop: 20,
+          paddingBottom: 20,
           borderTopWidth: 0,
           position: 'absolute',
           bottom: 25,
@@ -28,14 +29,29 @@ export default function TabNavigator({ navigation }: Props) {
         headerShown: false,
       }}>
       <Tab.Screen
-        name="Home"
+        name="Map"
         component={Home}
         options={{
-          title: 'Home',
-          //   tabBarIcon: ({ color }) => <Home2 color={color} />,
+          tabBarIcon: ({ color }) => <Map color={color} />,
+          tabBarLabelStyle: { marginTop: 10, fontSize: 14 },
         }}
       />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => <ProfileCircle color={color} />,
+          tabBarLabelStyle: { marginTop: 10, fontSize: 14 },
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: ({ color }) => <Setting3 color={color} />,
+          tabBarLabelStyle: { marginTop: 10, fontSize: 14 },
+        }}
+      />
     </Tab.Navigator>
   );
 }
