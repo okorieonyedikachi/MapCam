b2291b1db330f89978143ec858fe3b163143bf6a
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-04-10 13:18:10 +0100

    tab and stack navigation

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,27 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
+// import { createStackNavigator } from '@react-navigation/stack';
 import TabNavigator from './tab-navigation';
-
-// import Home from './(tabs)/home';
-// import Settings from './(tabs)/settings';
-
-export type RootStackParamList = {
-  TabNavigator: undefined;
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
 
 export default function RootStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="TabNavigator">
-        <Stack.Screen
-          name="TabNavigator"
-          component={TabNavigator}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      <TabNavigator />
     </NavigationContainer>
   );
 }
--- navigation/tab-navigation.tsx
+++ navigation/tab-navigation.tsx
@@ -1,17 +1,29 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StackScreenProps } from '@react-navigation/stack';
+// import { StackScreenProps } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Map, ProfileCircle, Setting3 } from 'iconsax-react-native';
 
-import { RootStackParamList } from '.';
+// import { RootStackParamList } from '.';
+import CameraScreen from 'screens/cameraScreen';
+
 import Home from './(tabs)/home';
 import Profile from './(tabs)/profile';
 import Settings from './(tabs)/settings';
 
 const Tab = createBottomTabNavigator();
 
-type Props = StackScreenProps<RootStackParamList, 'TabNavigator'>;
+const Stack = createStackNavigator();
 
-export default function TabNavigator({ navigation }: Props) {
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Stack.Screen name="Camera" component={CameraScreen} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
+}
+
+export default function TabNavigator() {
   return (
     <Tab.Navigator
       screenOptions={{
@@ -30,7 +42,7 @@ export default function TabNavigator({ navigation }: Props) {
       }}>
       <Tab.Screen
         name="Map"
-        component={Home}
+        component={StackNavigator}
         options={{
           tabBarIcon: ({ color }) => <Map color={color} />,
           tabBarLabelStyle: { marginTop: 10, fontSize: 14 },
