cb0e2b599b05d4aa44003f47ffd3d24a5b1e1230
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-04-16 17:54:53 +0100

    settings screen

--- navigation/(tabs)/settings.tsx
+++ navigation/(tabs)/settings.tsx
@@ -1,16 +1,32 @@
-import { StyleSheet, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Settings = () => {
   // const settingList = ['notification', 'account', 'help', 'about'];
 
   return (
-    <SafeAreaView>
-      <Text>Settings</Text>
-    </SafeAreaView>
+    <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0)' }}>
+      <View style={styles.header}>
+        <Text>Settings</Text>
+      </View>
+      <View style={styles.container} />
+    </View>
   );
 };
 
 export default Settings;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: 'orange',
+    height: '25%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 60,
+  },
+  container: {
+    borderRadius: 60,
+    flex: 1,
+    backgroundColor: 'white',
+    marginTop: 10,
+  },
+});
