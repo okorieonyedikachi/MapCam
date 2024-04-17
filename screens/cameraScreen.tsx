7e9a8c5393314b0bcd5a1a97319812677943a30e
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-04-17 21:19:58 +0100

    go back to home screen button

--- screens/cameraScreen.tsx
+++ screens/cameraScreen.tsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { ArrowLeft2 } from 'iconsax-react-native';
+import { StackParamList } from 'navigation/tab-navigation';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+type cameraScreenNavigationProps = StackNavigationProp<StackParamList, 'CameraScreen'>;
 
 const CameraScreen = () => {
+  const navigation = useNavigation<cameraScreenNavigationProps>();
+
   return (
-    <View style={{ justifyContent: 'center' }}>
+    <SafeAreaView style={{ alignItems: 'center', backgroundColor: 'red', flex: 1 }}>
+      <Pressable onPress={() => navigation.navigate('Home')}>
+        <ArrowLeft2 />
+      </Pressable>
       <Text>CameraScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
