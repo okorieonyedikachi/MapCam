26d7f62a315f3efb393a94381a374859c0e40372
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-04-19 14:51:16 +0100

    Camera layout and shutter button

--- screens/cameraScreen.tsx
+++ screens/cameraScreen.tsx
@@ -3,7 +3,6 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { ArrowLeft2 } from 'iconsax-react-native';
 import { StackParamList } from 'navigation/tab-navigation';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 type cameraScreenNavigationProps = StackNavigationProp<StackParamList, 'CameraScreen'>;
 
@@ -11,33 +10,67 @@ const CameraScreen = () => {
   const navigation = useNavigation<cameraScreenNavigationProps>();
 
   return (
-    <SafeAreaView style={{ alignItems: 'center', flex: 1 }}>
-      <View style={{ left: 0, width: '100%', paddingBottom: 10, paddingLeft: 10 }}>
-        <Pressable onPress={() => navigation.navigate('Home')}>
+    <View style={{ alignItems: 'center', flex: 1, backgroundColor: 'black' }}>
+      <View style={styles.camera}>
+        <Pressable onPress={() => navigation.navigate('Home')} style={styles.backBtn}>
           <ArrowLeft2 color="orange" />
         </Pressable>
-      </View>
-      <View style={styles.camera}>
+
         <Text>Camera</Text>
       </View>
       <View style={styles.camControls}>
-        <Text>retake</Text>
+        <Pressable style={styles.snapBtn}>
+          <View style={styles.snapBtnInner} />
+        </Pressable>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
 export default CameraScreen;
 
 const styles = StyleSheet.create({
+  backBtn: {
+    left: 0,
+    width: 50,
+    height: 50,
+    marginLeft: 20,
+    backgroundColor: 'rgba(218, 223, 225, .4)',
+    position: 'absolute',
+    top: 80,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   camera: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'white',
     width: '100%',
+    borderBottomLeftRadius: 70,
+    borderBottomRightRadius: 70,
     // aspectRatio: 4 / 3,
+    // position: 'absolute',
   },
   camControls: {
     width: '100%',
-    backgroundColor: 'black',
+    flexDirection: 'row',
+    height: 130,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  snapBtn: {
+    backgroundColor: 'orange',
+    width: 85,
+    height: 85,
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  snapBtnInner: {
+    width: 72,
+    height: 72,
+    backgroundColor: 'white',
+    borderRadius: 120,
+    margin: 'auto',
   },
 });
