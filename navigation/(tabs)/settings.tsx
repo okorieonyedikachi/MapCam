806b1b8080a416f5598a9208fec907d6f578c75b
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-04-19 14:50:49 +0100

    setting screen

--- navigation/(tabs)/settings.tsx
+++ navigation/(tabs)/settings.tsx
@@ -1,14 +1,36 @@
+import { More, Notification, Profile } from 'iconsax-react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+const SettingsList = [
+  { icon: <Profile color="orange" />, title: 'Profile' },
+  { icon: <Notification color="orange" />, title: 'Notification' },
+  { icon: <More color="orange" />, title: 'More' },
+];
+
+type renderItemsProps = {
+  title: string;
+  icon: React.ReactElement;
+};
+const RenderItems = ({ title, icon }: renderItemsProps) => (
+  <View style={styles.listContainer}>
+    <View>{icon}</View>
+    <Text>{title}</Text>
+  </View>
+);
 
 const Settings = () => {
-  // const settingList = ['notification', 'account', 'help', 'about'];
-
   return (
     <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0)' }}>
       <View style={styles.header}>
         <Text>Settings</Text>
       </View>
-      <View style={styles.container} />
+      <View style={styles.container}>
+        <FlatList
+          data={SettingsList}
+          renderItem={({ item }) => <RenderItems title={item.title} icon={item.icon} />}
+        />
+      </View>
     </View>
   );
 };
@@ -24,9 +46,9 @@ const styles = StyleSheet.create({
     borderRadius: 60,
   },
   container: {
-    borderRadius: 60,
     flex: 1,
     backgroundColor: 'white',
     marginTop: 10,
   },
+  listContainer: {},
 });
