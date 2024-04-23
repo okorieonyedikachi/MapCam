53e2efbf2ca8703c3f91eb942dc3162151d3808c
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-04-23 14:16:28 +0100

    uninstalled pkg

--- screens/OnboardingScreen.tsx
+++ screens/OnboardingScreen.tsx
@@ -1,24 +1,28 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { StackParamList } from 'navigation/tab-navigation';
+import React from 'react';
 import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 type OnboardingScreenNavigationProps = StackNavigationProp<StackParamList, 'Onboarding'>;
 const OnboardingScreen = () => {
   const navigation = useNavigation<OnboardingScreenNavigationProps>();
-
+  const navigateHome = () => {
+    navigation.navigate('App');
+  };
   return (
-    <SafeAreaView>
-      <Text>OnboardingScreen</Text>
-      <View>
-        <Pressable onPress={() => navigation.navigate('App')}>
-          <Text>Home</Text>
-        </Pressable>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Pressable onPress={navigateHome}>
+        <Text>Home</Text>
+      </Pressable>
+    </View>
   );
 };
 
 export default OnboardingScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
