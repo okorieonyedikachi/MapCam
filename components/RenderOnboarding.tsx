5ce897d0f93fe55ff8422797312d5fd924a80399
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-04-29 12:03:28 +0100

    expo font : Bubblegum-Sans

--- components/RenderOnboarding.tsx
+++ components/RenderOnboarding.tsx
@@ -1,14 +1,22 @@
-import { OnboardingData, OnboardingDataType } from 'lib/data';
+import { useFonts } from 'expo-font';
+import { OnboardingDataType } from 'lib/data';
 import LottieView from 'lottie-react-native';
 import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
-
 interface Props {
   item: OnboardingDataType;
   index: number;
 }
 
 const RenderOnboarding = ({ item, index }: Props) => {
+  const [fontsLoaded] = useFonts({
+    'SourceCodePro-Bubblegum-Sans': require('../assets/fonts/BubblegumSans-Regular.ttf'),
+  });
+
   const { width: SCREEN_WIDTH } = useWindowDimensions();
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
   return (
     <View
       style={[
@@ -23,7 +31,7 @@ const RenderOnboarding = ({ item, index }: Props) => {
           loop
         />
       </View>
-      <View>
+      <View style={styles.textContainer}>
         <Text style={[styles.itemsHeader, { color: item.textColor }]}>{item.title}</Text>
         <Text style={[styles.itemText, { color: item.textColor }]}>{item.text}</Text>
       </View>
@@ -35,16 +43,25 @@ export default RenderOnboarding;
 
 const styles = StyleSheet.create({
   itemContainer: {
-    flex: 1,
-    justifyContent: 'space-around',
+    paddingVertical: 120,
+    paddingHorizontal: 20,
     alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  textContainer: {
+    gap: 20,
+    paddingVertical: 50,
   },
   itemsHeader: {
     textAlign: 'center',
-    fontSize: 30,
+    fontSize: 50,
+    fontWeight: '500',
+    fontFamily: 'SourceCodePro-Bubblegum-Sans',
   },
   itemText: {
     textAlign: 'center',
-    fontSize: 18,
+    fontSize: 20,
+    lineHeight: 28,
+    fontFamily: 'SourceCodePro-Bubblegum-Sans',
   },
 });
