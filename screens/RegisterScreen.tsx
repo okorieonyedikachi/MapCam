c8c5705de80a48747b43f5ebd2255d6f6d1b4add
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-05-20 15:23:04 +0100

    new : Register component

---
+++ screens/RegisterScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from 'react-native';
+
+const RegisterScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>Hello</View>
+    </SafeAreaView>
+  );
+};
+
+export default RegisterScreen;
