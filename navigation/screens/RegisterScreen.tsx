83b75755eddd4b7ac45d8a30e9780660d65de1f4
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-05-24 10:04:39 +0100

    SignUp screen

--- navigation/screens/RegisterScreen.tsx
+++ navigation/screens/RegisterScreen.tsx
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { SafeAreaView, StyleSheet, TextInput, Text, View, useWindowDimensions } from 'react-native';
 
 const RegisterScreen = () => {
+  const { height: SCREEN_HEIGHT } = useWindowDimensions();
   return (
-    <View style={styles.mainContainer}>
-      <Text>Hello</Text>
-    </View>
+    <SafeAreaView style={[styles.mainContainer, { height: SCREEN_HEIGHT }]}>
+      <Text style={styles.contentTitle}>LETS GET STARTED</Text>
+      <View style={styles.contentContainer}>
+        <TextInput
+          style={styles.inputField}
+          autoCapitalize="none"
+          placeholder="Email"
+          keyboardType="email-address"
+        />
+        <TextInput style={styles.inputField} autoCapitalize="none" placeholder="Password" />
+        <TextInput style={styles.inputField} autoCapitalize="none" placeholder="Confirm Password" />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -13,6 +23,28 @@ export default RegisterScreen;
 
 const styles = StyleSheet.create({
   mainContainer: {
-    backgroundColor: 'red',
+    backgroundColor: '#fffaeb',
+    justifyContent: 'center',
+  },
+  contentTitle: {
+    color: 'orange',
+    fontFamily: 'BubblegumSans',
+    fontSize: 30,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  contentContainer: {
+    padding: 20,
+  },
+  inputField: {
+    padding: 20,
+    height: 40,
+    marginVertical: 12,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderRadius: 10,
+    borderBottomColor: 'grey',
+    fontSize: 18,
+    fontFamily: 'BubblegumSans',
   },
 });
