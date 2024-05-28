85a363f92ff20a16a9c6bdcd598f7b689218908e
Okorie Onyedikachi Daniella <cosmasdaniella@gmail.com>
2024-05-28 10:24:11 +0100

    UI: Password Input component

---
+++ components/PasswordInput.tsx
@@ -0,0 +1,53 @@
+import { Eye, EyeSlash } from 'iconsax-react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, TextInput, View } from 'react-native';
+
+interface PaswordInputProps {
+  value: string;
+  onChangeText: (e: any) => void;
+  onBlur: (e: any) => void;
+  placeholder: string;
+}
+const PaswordInput = ({ value, onChangeText, onBlur, placeholder }: PaswordInputProps) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const handlePasswordVisibility = () => {
+    setIsPasswordVisible((isPasswordVisible) => !isPasswordVisible);
+  };
+  return (
+    <View>
+      <TextInput
+        style={styles.inputField}
+        autoCapitalize="none"
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        onBlur={onBlur}
+        secureTextEntry={!isPasswordVisible}
+      />
+      <Pressable onPress={handlePasswordVisibility}>
+        {isPasswordVisible ? (
+          <EyeSlash color="orange" style={{ position: 'absolute', right: 20, bottom: 30 }} />
+        ) : (
+          <Eye color="orange" style={{ position: 'absolute', right: 20, bottom: 30 }} />
+        )}
+      </Pressable>
+    </View>
+  );
+};
+
+export default PaswordInput;
+
+const styles = StyleSheet.create({
+  inputField: {
+    padding: 10,
+    height: 60,
+    marginVertical: 12,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderRadius: 10,
+    borderBottomColor: 'grey',
+    fontSize: 18,
+    fontFamily: 'BubblegumSans',
+  },
+});
